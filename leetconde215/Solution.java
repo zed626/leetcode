f3a41fb0461110f83be7da1f4ec6b4ef2c504624
zed626 <425719071@qq.com>
2018-07-13 12:28:28 +0800

    second commit

--- leetconde215/Solution.java
+++ leetconde215/Solution.java
@@ -1,30 +1,34 @@
 package leetconde215;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 class Solution {
-    public int rob(int[] nums) {
-        if(nums==null || nums.length==0) return 0;
-        if(nums.length==1) return nums[0];
-        int [] count=  new int[nums.length];
-        count[0] = nums[0];
-        count[1] = Math.max(nums[0], nums[1]);
-        for(int i=2;i<nums.length-1;i++) {
-        	count[i] = Math.max(count[i-1], count[i-2]+nums[i]);
-        }
-        
-        int [] counts=  new int[nums.length];
-        counts[0] = nums[nums.length-1];
-        counts[1] = Math.max(nums[nums.length-1], nums[nums.length-2]);
-        for(int i=2;i<nums.length-1;i++) {
-        	counts[i] = Math.max(counts[i-1], counts[i-2]+nums[nums.length-i-1]);
-        }
-        return Math.max(count[nums.length-2],counts[nums.length-2]);
+    public int findKthLargest(int[] nums, int k) {
+        return part(nums,nums.length-k, 0,nums.length-1);
     }
-}
+    
+    public int part(int[] nums,int index, int start,int end) {
+    	int temp = nums[start];
+    	int i =start;
+    	int j = end;
+    	while(i<j) {
+    		while(i<j && nums[j]>=temp) j--;
+    		if(i<j) {
+    			nums[i++]=nums[j];
+    		}
+    		
+    		while(i<j && nums[i]<temp) i++;
+    		if(i<j) {
+    			nums[j--]=nums[i];
+    		}
+    	}
+    	nums[i]=temp;
+    	if(i==index) {
+    		return temp;
+    	}
+    	else if(i<index) {
+    		return part(nums, index, i+1, end);
+    	}
+    	else {
+    		return part(nums, index, start, i-1);
+    	}
+    }
+};
