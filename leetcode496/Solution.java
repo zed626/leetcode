eb83bf46fafe2bf61c1024490d92c296928a71b8
zed <425719071@qq.com>
2018-08-19 16:32:14 +0800

    8.19modify

--- leetcode496/Solution.java
+++ leetcode496/Solution.java
@@ -1,28 +1,6 @@
 package leetcode496;
 
-<<<<<<< HEAD
-import java.util.Arrays;
 
-class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        if(nums1==null || nums2==null) return new int[] {};
-        int [] result=new int[nums1.length];
-        Arrays.sort(nums2);
-        for(int i=0;i<nums1.length;i++) {
-        	int left=0,right=nums2.length;
-        	while(left<right) {
-        		int mid=left+(right-left)/2;
-        		if(mid<=nums1[i]) left=mid+1;
-        		else {
-					right=mid;
-				}
-        	}
-        	result[i]=nums2[left]>nums1[i]?nums2[left]:-1;
-        }
-        return result;
-    }
-}
-=======
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         if(nums1==null || nums2==null) return new int[] {};
@@ -55,4 +33,4 @@ class Solution {
         findNums[i] = map.getOrDefault(findNums[i], -1);
     return findNums;
 }*/
->>>>>>> b182e05a3935e169388638c80776b5ba0c629cf8
+
